afb7ec1cb8b079cfb47dd3681f03b7e334b6b685
Fernando Monteiro <fernando.monteiro.f11@gmail.com>
2022-05-26 11:32:00 -0300

    Separa a mensagem em 2 partes caso passe o limite de 2000 caracteres

--- src/utils/sendMessage.ts
+++ src/utils/sendMessage.ts
@@ -1,6 +1,13 @@
 import { Message } from 'discord.js';
 
 const sendMessage = (message: Message, str: string) => {
+  if (str.length > 2000) {
+    const str1 = str.slice(1, 2000);
+    const str2 = str.slice(2000);
+    message.reply(str1);
+    message.reply(str2);
+    return;
+  }
   return message.reply(str);
 };
 
